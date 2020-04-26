d5e3c73bd54fd97f30c79b44c1d8f07586cf341d
Akito Nagai <a.nagai@mof-mof.co.jp>
2020-04-26 17:42:19 +0900

    タスクモデルを作成する

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 class Task < ApplicationRecord
+  enum status: {
+    untouched: 0,
+    started: 1,
+    completed: 2
+  }
+
+  enum priority: {
+    top: 0,
+    next: 1,
+    other: 2
+  }
 end
